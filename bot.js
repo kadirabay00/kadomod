57b60c3d533bb326c6d3f73d8961d1cd0ac7727f
kadirabay00 <69356030+kadirabay00@users.noreply.github.com>
2021-12-18 04:53:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -158,7 +158,15 @@ client.on("message", async message => {
 //------------------------------------------------------------------------------------------------------------\\
 
 
-
+client.on('voiceStateUpdate', async (oldState, newState) => {
+    if (!oldState.channelID && newState.channelID) return newState.guild.channels.cache.get(ayarlar.voicelog).send(`${newState.guild.members.cache.get(newState.id).displayName} üyesi \`${newState.guild.channels.cache.get(newState.channelID).name}\` adlı sesli kanala girdi!`);
+    if (oldState.channelID && !newState.channelID) return newState.guild.channels.cache.get(ayarlar.voicelog).send(`${newState.guild.members.cache.get(newState.id).displayName} üyesi \`${newState.guild.channels.cache.get(oldState.channelID).name}\` adlı sesli kanaldan ayrıldı!`);
+    if (oldState.channelID && newState.channelID && oldState.channelID != newState.channelID) return newState.guild.channels.cache.get(ayarlar.voicelog).send(`${newState.guild.members.cache.get(newState.id).displayName} üyesi ses kanalını değiştirdi! (\`${newState.guild.channels.cache.get(oldState.channelID).name}\` - \`${newState.guild.channels.cache.get(newState.channelID).name}\`)`);
+    if (oldState.channelID && oldState.selfMute && !newState.selfMute) return newState.guild.channels.cache.get(ayarlar.voicelog).send(`${newState.guild.members.cache.get(newState.id).displayName} üyesi \`${newState.guild.channels.cache.get(newState.channelID).name}\` adlı sesli kanalda kendi susturmasını kaldırdı!`);
+    if (oldState.channelID && !oldState.selfMute && newState.selfMute) return newState.guild.channels.cache.get(ayarlar.voicelog).send(`${newState.guild.members.cache.get(newState.id).displayName} üyesi \`${newState.guild.channels.cache.get(newState.channelID).name}\` adlı sesli kanalda kendini susturdu!`);
+    if (oldState.channelID && oldState.selfDeaf && !newState.selfDeaf) return newState.guild.channels.cache.get(ayarlar.voicelog).send(`${newState.guild.members.cache.get(newState.id).displayName} üyesi \`${newState.guild.channels.cache.get(newState.channelID).name}\` adlı sesli kanalda kendi sağırlaştırmasını kaldırdı!`);
+    if (oldState.channelID && !oldState.selfDeaf && newState.selfDeaf) return newState.guild.channels.cache.get(ayarlar.voicelog).send(`${newState.guild.members.cache.get(newState.id).displayName} üyesi \`${newState.guild.channels.cache.get(newState.channelID).name}\` adlı sesli kanalda kendini sağırlaştırdı!`);
+});
 
 
 //------------------------------------------------------------------------------------------------------------\\
